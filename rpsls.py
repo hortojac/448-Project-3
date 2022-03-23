1a2f5664b305f128370c16d9d6712543a48a272a
= <l351s450@ku.edu>
2022-03-23 18:04:26 -0500

    Added Assets folder for organization

--- rpsls.py
+++ rpsls.py
@@ -1,20 +1,33 @@
 from tkinter import *
 from tkinter.font import BOLD, Font
 from functools import partial   
+from PIL import ImageTk, Image
+
+def show_frame(frame):
+    frame.tkraise()
+
 
 root = Tk()
 root.state('zoomed') #puts the window mode in zoomed
 root.title("Rock Paper Scissors Lizard Spock") #labels our frame
 
-def show_frame(frame):
-    frame.tkraise()
+
+#Initialize start image for use
+
+image=Image.open("assets/rpsls/start.jpeg") 
+#img=image.resize((40,40))
+img_start=ImageTk.PhotoImage(image) #image for just the start button (bg set to blue)
+
+image=Image.open("assets/rpsls/rules.png") 
+#img=image.resize((40,40))
+img_start=ImageTk.PhotoImage(image) #image for just the start button (bg set to blue)
 
 frame1 = Frame(root) #welcome menu frame
 
 frame2 = Frame(root) # main gameplay frame
 
 for frame in (frame1, frame2):
-    frame.grid(row=0, column=0, sticky = 'nsew') #frame 1 - 10 rows and columns initiliazed to 0 and widgest are sticky.
+    frame.grid(row=0, column=0, sticky = 'nsew') #frames' rows and columns initiliazed to 0 and widgets are sticky.
 
 
 show_frame(frame1)
@@ -28,6 +41,10 @@ frame1_startBtn.grid()#place(relx=.5, rely=.5, anchor=CENTER)
 
 frame1_exitBtn = Button(frame1, text="Exit",font=("Arial",50, BOLD), command=partial(root.destroy)) #exits the program
 frame1_exitBtn.grid()#place(relx=.5, rely=.5, anchor=CENTER)
+
+##
+myLabel1 = Label(frame1, text="Battleship!\nPress start to begin playing.",font=("Arial", 25)).place(relx=.5, rely=.2,anchor= CENTER)
+frame1_button = Button(frame1, text="Start",font=("Arial",70, BOLD), command=partial(show_frame,frame2), bg="white", padx=20,pady=20, image=img_start, compound=CENTER).place(relx=.50, rely=.5,anchor= CENTER)
 
 #frame2 code
 frame2_txt = Label(frame2, text="Gameplay Coming Soon!",font=("Arial", 25))
