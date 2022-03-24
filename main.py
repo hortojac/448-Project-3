b49142ac1968760c1970c119aebbd0e5f1566225
= <l351s450@ku.edu>
2022-03-23 19:11:54 -0500

    main menu works!!

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ frame1 = Frame(root)
 #for frame in (frame1):
 frame1.grid(row=0, column=0, sticky = 'nsew') #frame 1 - 10 rows and columns initiliazed to 0 and widgest are sticky.
 
-# connect4_button  = Button(frame1, text="Connect 4",font=("Arial",20, BOLD), padx=25, pady=25, command=partial()).grid() #place(relx=.5,rely=.3,anchor= CENTER) 
-rpsls_button = Button(frame1, text="RPSLS",font=("Arial",20, BOLD), padx=25, pady=25, command=runRPSLS).grid() #place(relx=.5,rely=.4,anchor= CENTER)
-# blackjack_button = Button(frame1, text="BlackJack",font=("Arial",20, BOLD), padx=25, pady=25, command=partial()).grid() #place(relx=.5,rely=.5,anchor= CENTER)
-# tictactoe_button = Button(frame1, text="Tic Tac Toe",font=("Arial",20, BOLD), padx=25, pady=25, command=partial()).grid() #place(relx=.5,rely=.6,anchor= CENTER)
-# wordle_button = Button(frame1, text="Wordle",font=("Arial",20, BOLD), padx=25, pady=25, command=partial()).grid() #place(relx=.5,rely=.7,anchor= CENTER) 
+connect4_button  = Button(frame1, text="Connect 4",font=("Arial",20, BOLD), padx=25, pady=25, command=lambda: os.system('python3 connect4.py')).grid() #place(relx=.5,rely=.3,anchor= CENTER) 
+rpsls_button = Button(frame1, text="RPSLS",font=("Arial",20, BOLD), padx=25, pady=25, command=lambda: os.system('python3 rpsls.py')).grid() #place(relx=.5,rely=.4,anchor= CENTER)
+blackjack_button = Button(frame1, text="BlackJack",font=("Arial",20, BOLD), padx=25, pady=25, command=lambda: os.system('python3 blackjack.py')).grid() #place(relx=.5,rely=.5,anchor= CENTER)
+tictactoe_button = Button(frame1, text="Tic Tac Toe",font=("Arial",20, BOLD), padx=25, pady=25, command=lambda: os.system('python3 tictactoe.py')).grid() #place(relx=.5,rely=.6,anchor= CENTER)
+wordle_button = Button(frame1, text="Wordle",font=("Arial",20, BOLD), padx=25, pady=25, command=lambda: os.system('python3 wordle.py')).grid() #place(relx=.5,rely=.7,anchor= CENTER) 
 
 
 
